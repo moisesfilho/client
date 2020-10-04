502501a1e4f4c897c8f3cf7aba27e8be758aa406
moisesfilho <moises.son@gmail.com>
2020-10-04 12:46:42 -0300

    Importando bootstrap como um módulo

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: 'dist'
+    publicPath: "dist",
   },
   module: {
     rules: [
@@ -22,6 +22,38 @@ module.exports = {
         use: {
           loader: "babel-loader",
         },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        use: "url-loader?limit=100000",
+      },
+      {
+        test: /\.png$/,
+        use: "url-loader?limit=100000",
+      },
+      {
+        test: /\.jpg$/,
+        use: "file-loader",
+      },
+      {
+        test: /\.(woff|woff2) (\?v=\d+\.\d+\.\d+)?$/,
+        use: "url-loader?limit=10000&mimetype=application/font-woff",
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        use: "url-loader?limit=10000&mimetype=application/octet-stream",
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        use: "file-loader",
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        use: "url-loader?limit=10000&mimetype=image/svg+xml",
       },
     ],
   },
